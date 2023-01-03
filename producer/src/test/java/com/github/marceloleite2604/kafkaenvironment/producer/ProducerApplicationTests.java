440df5049635990fe1769df61686c49f9d388ed8
MarceloLeite2604 <marceloleite2604@gmail.com>
2023-01-03 15:11:38 -0300

    Create producer project

---
+++ producer/src/test/java/com/github/marceloleite2604/kafkaenvironment/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marceloleite2604.kafkaenvironment.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
